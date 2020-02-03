a2a124fb4bc188defc26dab10288c4da300bdfc9
Jennifer Kroll <jenniferbantakroll@gmail.com>
2020-02-03 00:35:59 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,13 +1,5 @@
 def my_hash
-  "#my_hash" do
-    it "uses the literal constructor to create a hash that contains key/value pairs" do 
-      expect(my_hash).to be_a(Hash)
-      expect(my_hash.keys.count).to_not eq(0)
-  end
-end
-
   
-end
 
 
 def shipping_manifest
