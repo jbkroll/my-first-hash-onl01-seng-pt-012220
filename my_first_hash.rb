1c22b7cb49b285e755894a75fbd79169bcebda51
Jennifer Kroll <jenniferbantakroll@gmail.com>
2020-02-03 00:36:35 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,3 @@
-require_relative 'spec_helper'
-require_relative '../my_first_hash.rb'
-
-
 context "Challenge I: Instantiating Hashes" do
   describe "#my_hash" do
     it "uses the literal constructor to create a hash that contains key/value pairs" do 
